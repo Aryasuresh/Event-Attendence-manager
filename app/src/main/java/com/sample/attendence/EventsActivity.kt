8aaf033a902f3584eea51e8325f00fc57f93fd6a
aryasuresh <aryasuresh159@gmail.com>
2018-07-26 15:26:46 +0530

    logout action added

--- app/src/main/java/com/sample/attendence/EventsActivity.kt
+++ app/src/main/java/com/sample/attendence/EventsActivity.kt
@@ -3,8 +3,10 @@ package com.sample.attendence
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.*
 
 import kotlinx.android.synthetic.main.activity_event.*
+import org.jetbrains.anko.intentFor
 
 class EventsActivity : AppCompatActivity() {
 
@@ -19,4 +21,32 @@ class EventsActivity : AppCompatActivity() {
         }
     }
 
-}
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val n= MenuInflater(this).inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId) {
+            R.id.logout -> {
+                val pref = getSharedPreferences("events", 0)
+                val editor = pref.edit()
+
+                editor.putString("access_token", "")
+                editor.apply()
+                startActivity(intentFor<LoginActivity>())
+                finish()
+
+            }
+            R.id.about -> {
+
+            }
+        }
+            return super.onOptionsItemSelected(item)
+
+    }
+
+
+
+    }
+
